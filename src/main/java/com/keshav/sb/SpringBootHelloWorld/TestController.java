6b7b6f8bb9649a6581e73c5082981744c6012fa0
keshavaero0552 <keshav.aero0552@gmail.com>
2017-06-06 14:34:27 -0500

    i am keshav java developer

---
+++ src/main/java/com/keshav/sb/SpringBootHelloWorld/TestController.java
@@ -0,0 +1,15 @@
+package com.keshav.sb.SpringBootHelloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/welcome.html")
+	public ModelAndView firstPage() {
+		return new ModelAndView("welcome");
+	}
+
+}
